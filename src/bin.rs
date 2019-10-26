cc9ed29ef5e318964d07845636a177f7f3553f64
0xSilene <0xsilene@MacBook.local>
2019-10-26 13:37:01 -0400

    Update bin.rs

--- src/bin.rs
+++ src/bin.rs
@@ -4,8 +4,6 @@ use winternitz_ots::wots;
 fn main() {
     // Have A Hexadecimal String You Would Like To Sign
     let hex_string: String = String::from("ECC4C3134F80E54C08BAAE1A3F3BDC07BB3AD3906FF62D0D3DFC1EE87AE83194");
-
-    println!("Input String: {}", hex_string);
     
     // Generate W-OTS Keypair | Get The Hash Of The Public Key (For An Address For Example) using a digest from 1-64
     let keypair = wots::generate_wots();
@@ -24,19 +22,25 @@ fn main() {
 
     // Signature Attributes
         // A Vector of The Input Into Its Corresponding Value From 0-15 (for w=16)
-    let input = signature.input;
-    let u4_bytes = signature.signature_cycles;
+    let input = &signature.input;
+    let u4_bytes = &signature.signature_cycles;
+    let is_pk_hash_real = signature.verify_public_key_hash(public_key_hash.clone());
 
+    println!();
     println!("PK[0]: {}",pk[0]);
     println!("PK[63]: {}",pk[63]);
     println!("SK[0]: {}",sk[0]);
     println!("SK[63]: {}",sk[63]);
     println!();
-    println!("Address: {}",public_key_hash);
+    println!("Public Key Address: 0x{}",public_key_hash);
+    println!("Hash: Blake2b");
     println!("w: {}",w);
     println!("n: {}",n);
+    println!();
     println!("Input: {}",input);
-    println!("Cycles: {:?}",u4_bytes);
+    println!("Cycles: {0:?}",u4_bytes);
     println!();
     println!("Is Signature Valid: {}",is_signature_valid);
+    println!("Is Public Key Address Valid For Given Public Key: {}",is_pk_hash_real);
+    println!();
 }
